2b98edbd0bb7776ca010f0d809fb9167fa5283b0
dosung <77737044+seongeunjang@users.noreply.github.com>
2022-02-10 22:24:28 +0900

    Make list and basic html and css

---
+++ main.js
@@ -0,0 +1,20 @@
+const inputBox = document.querySelector("#inputBox");
+const time = document.querySelector("#time");
+const addBtn = document.querySelector("#addBtn");
+const listBox = document.querySelector("#listBox");
+const morning = document.querySelector("#morning");
+const afternoon = document.querySelector("#afternoon");
+const night = document.querySelector("#night");
+
+addBtn.addEventListener("click", () => {
+  const list = document.createElement("li");
+  list.innerText = `${inputBox.value}`;
+  if (time.value == "Morning") {
+    morning.appendChild(list);
+  } else if (time.value == "Afternoon") {
+    afternoon.appendChild(list);
+  } else {
+    night.appendChild(list);
+  }
+  inputBox.value = "";
+});
