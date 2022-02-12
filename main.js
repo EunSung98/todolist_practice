1f6f4b99def6c1bcbae93260228ed6415ecad151
seongeun <jangseongeun752@gmail.com>
2022-02-12 22:32:45 +0900

    Do not add blank list and make list...(should fix)

--- main.js
+++ main.js
@@ -1,21 +1,39 @@
 const inputBox = document.querySelector("#inputBox");
 const time = document.querySelector("#time");
 const addBtn = document.querySelector("#addBtn");
-const listBox = document.querySelector("#listBox");
 const morning = document.querySelector("#morning");
 const afternoon = document.querySelector("#afternoon");
 const night = document.querySelector("#night");
+const miniList = document.querySelectorAll(".miniList");
 
-addBtn.addEventListener("click", () => {
+function addList() {
   const list = document.createElement("li");
+  const listTime = document.createElement("div");
+  listTime.innerText = `${time.value}`;
   list.innerText = `${inputBox.value}`;
   if (time.value == "Morning") {
+    if (morning.childElementCount === 0) {
+      morning.appendChild(listTime);
+    }
     morning.appendChild(list);
   } else if (time.value == "Afternoon") {
+    if (afternoon.childElementCount === 0) {
+      afternoon.appendChild(listTime);
+    }
     afternoon.appendChild(list);
   } else {
+    if (night.childElementCount === 0) {
+      night.appendChild(listTime);
+    }
     night.appendChild(list);
   }
   inputBox.value = "";
   inputBox.focus();
+}
+
+addBtn.addEventListener("click", () => {
+  if (inputBox.value.trim() == "") {
+    return;
+  }
+  addList();
 });
